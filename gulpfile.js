81415d74a75fc8fdc4824d14e01801df4a0f3752
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-22 16:54:12 -0500

    fix: js compiled file name

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const cache = require('gulp-cache');
@@ -33,7 +32,6 @@ function javascript() {
     .pipe(concat('app.js'))
     .pipe(terser())
     .pipe(sourcemaps.write('.'))
-    .pipe(rename({ suffix: '.min' }))
     .pipe(dest('./public/build/js'));
 }
 
--- views/layout.php
+++ views/layout.php
@@ -7,7 +7,7 @@
   <title>App Salón</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="build/css/app.css">
-  <script defer src="build/js/app.min.js"></script>
+  <script defer src="build/js/app.js"></script>
 </head>
 
 <body>
