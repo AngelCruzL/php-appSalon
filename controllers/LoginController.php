1b9d41b119ed97b7cdbde208eaa0b0b1339d28fd
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-07-22 14:30:15 -0500

    feat: add auth routes

---
+++ controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Controllers;
+
+class LoginController
+{
+  public static function login()
+  {
+    echo 'LoginController::index';
+  }
+
+  public static function logout()
+  {
+    echo 'LoginController::logout';
+  }
+
+  public static function createAccount()
+  {
+    echo 'LoginController::createAccount';
+  }
+
+  public static function forgotPassword()
+  {
+    echo 'LoginController::forgotPassword';
+  }
+
+  public static function resetPassword()
+  {
+    echo 'LoginController::resetPassword';
+  }
+}
--- public/index.php
+++ public/index.php
@@ -3,11 +3,19 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 
 $router = new Router();
 
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+$router->get('/crear-cuenta', [LoginController::class, 'createAccount']);
+$router->post('/crear-cuenta', [LoginController::class, 'createAccount']);
+$router->get('/olvide', [LoginController::class, 'forgotPassword']);
+$router->post('/olvide', [LoginController::class, 'forgotPassword']);
+$router->get('/recuperar', [LoginController::class, 'resetPassword']);
+$router->post('/recuperar', [LoginController::class, 'resetPassword']);
 
-
-// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->checkRoutes();
