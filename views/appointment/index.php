11e0229c8f98919943e1f374b3609e7544e21f40
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-08-07 17:38:53 -0500

    feat: add tabs component

--- views/appointment/index.php
+++ views/appointment/index.php
@@ -2,6 +2,12 @@
 <p class="pageDescription">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+  <nav class="tabs mb-4">
+    <button class="active" type="button" data-step="1">Servicios</button>
+    <button type="button" data-step="2">Información de la cita</button>
+    <button type="button" data-step="3">Resumen</button>
+  </nav>
+
   <div id="step-1" class="section">
     <h2>Servicios</h2>
     <p>Elige tus servicios a continuación</p>
