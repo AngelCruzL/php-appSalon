15e500dca65344a9db6272aef709b9d581507e6d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-08-11 00:21:34 -0500

    feat: add admin navbar

--- views/templates/top-bar.php
+++ views/templates/top-bar.php
@@ -2,3 +2,11 @@
   <p>Hola <strong><?php echo $name; ?></strong></p>
   <a class="btn" href="/logout">Cerrar sesión</a>
 </div>
+
+<?php if (isset($_SESSION['isAdmin'])) : ?>
+  <div class="topBar-services">
+    <a class="btn" href="/admin">Ver Citas</a>
+    <a class="btn" href="/servicios">Ver Servicios</a>
+    <a class="btn" href="/servicios/crear">Nuevo Servicio</a>
+  </div>
+<?php endif; ?>
