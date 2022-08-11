ad1ae44a05b113a9e31e037df7eac3ee53f09982
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-08-11 11:00:53 -0500

    feat: setting up deployment

--- Router.php
+++ Router.php
@@ -21,7 +21,7 @@ class Router
   {
     session_start();
 
-    $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+    $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
     $httpMethod = $_SERVER['REQUEST_METHOD'];
 
     if ($httpMethod === 'GET') {
